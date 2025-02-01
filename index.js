307e9a48ddc172ed14b166ba109e53245114ea2e
cod-emminex <emminexy@yahoo.com>
2025-02-01 22:19:45 +0100

    HNG stage 0 task additions and deletions

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   const response = {
-    email: "your-email@example.com", // Replace with your email
+    email: "emminexy@yahoo.com",
     current_datetime: new Date().toISOString(),
-    github_url: "https://github.com/yourusername/your-repo", // Replace with your GitHub repo URL
+    github_url: "https://github.com/cod-emminex/hng12-backend-task",
   };
   res.status(200).json(response);
 });
